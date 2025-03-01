60cbc86d1d25cdadbf4026c26ea965ab16976e53
Javier Santos <javsanmar5@alum.us.es>
2025-03-01 12:51:41 +0100

    Add Expo Router as dependency

---
+++ frontend/app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Page() {
+  return (
+    <View> 
+        <Text>Hello world, frontend is working. :D</Text>
+    </View>
+  );
+}
+
